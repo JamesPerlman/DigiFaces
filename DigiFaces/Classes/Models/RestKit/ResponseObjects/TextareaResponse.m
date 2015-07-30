253abea6b3861a942d7c0b5d5df868787d5929c8
JamesPerlman <jam.e.perl@gmail.com>
2015-07-29 23:39:17 -0500

    ....

---
+++ DigiFaces/Classes/Models/RestKit/ResponseObjects/TextareaResponse.m
@@ -0,0 +1,14 @@
+//
+//  TextareaResponse.m
+//  DigiFaces
+//
+//  Created by James on 7/29/15.
+//  Copyright (c) 2015 INET360. All rights reserved.
+//
+
+#import "TextareaResponse.h"
+
+@implementation TextareaResponse
+
+
+@end
